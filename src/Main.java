dd94516a71b2ba1193b49c730852ae220739604b
Amiens81 <ypovelica@ya.ru>
2024-06-22 23:08:21 +0300

    First test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main{
     public static void main(String[] args){
         int a = 10;
         int b = 5;
-        int c = a + b;
-System.out.println(c + " Welcome to new life!");
+        System.out.println(c + " Welcome to new life!");
     }
 }
